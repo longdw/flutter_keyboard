2475c18bba21bccc3b032b13eb620c886722528b
longdw <342662033@qq.com>
2019-08-16 10:17:42 +0800

    init

---
+++ lib/home.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'this is a project to test keyboard',
+        ),
+
+        Flexible(
+          fit: FlexFit.tight,
+          child: CustomScrollView(
+            slivers: <Widget>[
+
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  width: 100,
+                  height: 160,
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  width: 100,
+                  height: 140,
+                  child: Container(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  width: 100,
+                  height: 120,
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Container(
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+
+              SliverToBoxAdapter(
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'this is first input text'
+                  ),
+                ),
+              ),
+
+              SliverToBoxAdapter(
+                child: Padding(padding: EdgeInsets.only(top: 20),),
+              ),
+
+              SliverToBoxAdapter(
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'this is second input text'
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        Container(
+          color: Colors.grey[400],
+          padding: EdgeInsets.all(8),
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              FlatButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(2)
+                  ),
+                  color: Colors.black,
+                  textColor: Colors.white,
+                  onPressed: () {
+                  },
+                  child: Text(
+                      'button1'
+                  )
+              ),
+              FlatButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(2)
+                  ),
+                  color: Colors.black,
+                  textColor: Colors.white,
+                  onPressed: () {
+
+                  },
+                  child: Text(
+                      'button2'
+                  )
+              ),
+              FlatButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(2)
+                  ),
+                  color: Colors.black,
+                  textColor: Colors.white,
+                  onPressed: () {
+
+                  },
+                  child: Text(
+                      'button3'
+                  )
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+}
